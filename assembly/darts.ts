c028bc0fc3e7f30aec8bfaebca29756d804c2976
Robert Rees <shudderfix@gmail.com>
2023-11-24 07:43:09 +0000

    Tweak darts to check more conditions

--- assembly/darts.ts
+++ assembly/darts.ts
@@ -1,4 +1,18 @@
 
-export function score(x: i32, y: i32): i32 {
+export function score(x: f32, y: f32): i32 {
+	const distanceFromCentre = Math.sqrt(Math.pow(Math.abs(x), 2) + Math.pow(Math.abs(y), 2));
+
+	if (distanceFromCentre <= 1) {
+		return 10;
+	}
+
+	if (distanceFromCentre <= 5) {
+		return 5;
+	}
+
+	if (distanceFromCentre <= 10) {
+		return 1;
+	}
+	
 	return 0;
 }
--- tests/darts.js
+++ tests/darts.js
@@ -3,5 +3,10 @@ import { score } from "../build/debug.js";
 
 
 assert.strictEqual(score(11, -11), 0);
+assert.strictEqual(score(0.5, 0.5), 10);
+assert.strictEqual(score(3, -2), 5);
+assert.strictEqual(score(6, -6), 1);
+assert.strictEqual(score(-9, 9), 0);
+assert.strictEqual(score(0.7, 0.7), 10);
 
 console.log("ok");
